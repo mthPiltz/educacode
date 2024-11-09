870c250fe66cc8a8ae08f869093d53981896879a
mthPiltz <matheus.piltz@ufms.br>
2024-11-09 08:24:14 -0400

    adicao do botao voltar

--- tamplates/share/header.js
+++ tamplates/share/header.js
@@ -3,7 +3,16 @@ const html = `
     <div class="header">
         <img src="../../../assets/logoEducacode.svg" alt="Logo do Educacode">
 
-        <img class="gif" src="../../../assets/iconeConfiguracoes.gif" alt="Icone de configurações">
+        <div style="display: flex; align-items: center;">
+            <img 
+                class="gif" src="../../../assets/iconeConfiguracoes.gif" 
+                alt="Icone de configurações">
+            <img
+                id="voltar" 
+                src="../../../assets/sair.png" 
+                alt="Icone para voltar" 
+                style="height: 80px; cursor: pointer;">
+        </div>
     </div>
 </header>
 `;
@@ -11,4 +20,13 @@ const html = `
 export default function header(){
     let div = document.getElementById("header");
     div.innerHTML = html;
+    adicionaAcoes()
+}
+
+function adicionaAcoes(){
+    let img = document.getElementById('voltar');
+
+    img.addEventListener('click', () => {
+        window.location.href = '/';
+    });
 }
