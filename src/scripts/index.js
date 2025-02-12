0238e2ca5e6d2fc93166ddaa29d926fbe654eac2
mthPiltz <matheus.piltz@ufms.br>
2025-02-11 21:17:49 -0400

    [fix] header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,18 @@
-import header from "/tamplates/share/header.js"
+const headerPath = window.location.host == "mthpiltz.github.io" 
+                                ? "educacode/tamplates/share/header.js"
+                                : "tamplates/share/header.js"
 
+async function loadHeader() {
+    const module = await import(`/${headerPath}`);
+    const header = module.default; // Pega a função exportada como default
+    header(); // Chama a função
+}
 
-header();
+loadHeader();
+
+// const script = document.createElement("script");
+// script.src = `./${headerPath}`;
+// script.type = "module";
+// document.body.appendChild(script);
+
+// header();
