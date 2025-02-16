af600a98dfe06b92dfb8f129f9843c13f1cb765a
mthPiltz <matheus.piltz@ufms.br>
2025-02-16 13:55:19 -0400

    [feature] implementacao do algoritmo na fase 2 geo

--- tamplates/geografia/2/fase2Geo.js
+++ tamplates/geografia/2/fase2Geo.js
@@ -1,5 +1,6 @@
 import modalParabens, { abrir } from "../../share/modalParabens.js"
 import header from "/tamplates/share/header.js"
+import Estados from "../estados.js"
 
 header();
 modalParabens();
@@ -10,242 +11,8 @@ let capi = {
     posicao : "SC"
 }
 
-const estados = [
-    {
-        sigla : "RS",
-        norte : "SC",
-        sul : null,
-        leste : null,
-        oeste : null,
-        top : 492,
-        right : 315
-    },
-    {
-        sigla : "SC",
-        norte : "PR",
-        sul : "RS",
-        leste : null,
-        oeste : null,
-        top : 464, 
-        right : 269
-    },
-    {
-        sigla : "PR",
-        norte : "SP",
-        sul : "SC",
-        leste : null,
-        oeste : "MS",
-        top : 432, 
-        right : 300
-    },
-    {
-        sigla : "SP",
-        norte : "MG",
-        sul : "PR",
-        leste : null,
-        oeste : "MS",
-        top : 403, 
-        right : 264
-    },
-    {
-        sigla : "MG",
-        norte : "BA",
-        sul : "SP",
-        leste : "ES",
-        oeste : "GO",
-        top : 364, 
-        right : 215
-    },
-    {
-        sigla : "ES",
-        norte : "BA",
-        sul : "RJ",
-        leste : null,
-        oeste : "MG",
-        top : 380, 
-        right : 150
-    },
-    {
-        sigla : "RJ",
-        norte : "ES",
-        sul : null,
-        leste : null,
-        oeste : "MG",
-        top : 423, 
-        right : 180
-    },
-    {
-        sigla : "MS",
-        norte : "MT",
-        sul : null,
-        leste : "SP",
-        oeste : null,
-        top : 385, 
-        right : 325
-    },
-    {
-        sigla : "MT",
-        norte : "PA", 
-        sul : "MS",
-        leste : "GO", 
-        oeste : "RO",
-        top : 310, 
-        right : 340
-    },
-    {
-        sigla : "GO",
-        norte : "TO",
-        sul : "MG", 
-        leste : "MG", 
-        oeste : "MT",
-        top : 325, 
-        right : 268
-    },
-    {
-        sigla : "AC",
-        norte : "AM",
-        sul : null, 
-        leste : "RO", 
-        oeste : null,
-        top : 280,  
-        right : 520
-    },
-    {
-        sigla : "AM",
-        norte : "RR",
-        sul : "RO", 
-        leste : "PA", 
-        oeste : null,
-        top : 205,  
-        right : 440
-    },
-    {
-        sigla : "RO",
-        norte : "AM",
-        sul : null, 
-        leste : "MT", 
-        oeste : "AC",
-        top : 285,  
-        right : 430
-    },
-    {
-        sigla : "AP",
-        norte : null,
-        sul : "PA", 
-        leste : null, 
-        oeste : null,
-        top : 115,   
-        right : 285
-    },
-    {
-        sigla : "RR",
-        norte : null,
-        sul : "AM", 
-        leste : "PA", 
-        oeste : null,
-        top : 110,  
-        right : 405
-    },
-    {
-        sigla : "PA",
-        norte : "AP",
-        sul : "MT", 
-        leste : "TO", 
-        oeste : "AM",
-        top : 195, 
-        right : 310
-    },
-    {
-        sigla : "TO",
-        norte : "MA",
-        sul : "GO", 
-        leste : "BA", 
-        oeste : "PA",
-        top : 270,  
-        right : 255
-    },
-    {
-        sigla : "BA",
-        norte : "PI",
-        sul : "MG", 
-        leste : "SE", 
-        oeste : "TO",
-        top : 295,  
-        right : 180 
-    },
-    {
-        sigla : "SE",
-        norte : "AL",
-        sul : "BA", 
-        leste : null, 
-        oeste : "BA",
-        top : 290,  
-        right : 115
-    },
-    {
-        sigla : "AL",
-        norte : "PE",
-        sul : "SE", 
-        leste : null, 
-        oeste : "BA",
-        top : 275,  
-        right : 95
-    },
-    {
-        sigla : "PB",
-        norte : "RN",
-        sul : "PE", 
-        leste : null, 
-        oeste : "CE",
-        top : 234,  
-        right : 105
-    },
-    {
-        sigla : "PE",
-        norte : "PB",
-        sul : "AL", 
-        leste : null, 
-        oeste : "PI",
-        top : 250,  
-        right : 140
-    },
-    {
-        sigla : "RN",
-        norte : null,
-        sul : "PB", 
-        leste : null, 
-        oeste : "CE",
-        top : 210,  
-        right : 105
-    },
-    {
-        sigla : "CE",
-        norte : null,
-        sul : "PE", 
-        leste : "RN", 
-        oeste : "PI",
-        top : 190,  
-        right : 145
-    },
-    {
-        sigla : "PI",
-        norte : null,
-        sul : "BA", 
-        leste : "CE", 
-        oeste : "MA",
-        top : 230, 
-        right : 185
-    },
-    {
-        sigla : "MA",
-        norte : null,
-        sul : "TO", 
-        leste : "PI", 
-        oeste : "PA",
-        top : 195,  
-        right : 121
-    },
-]
+const estados = Estados();
+let caminho = [];
 
 let capiIMG = document.getElementById("capi");
 
@@ -261,9 +28,12 @@ input.addEventListener('keypress', (event) => {
 
                 
                 if(estadoAtual[0].norte){
+                    caminho.push({
+                        posicao: estadoAtual[0].norte,
+                        top: `${proxEstado[0].top}px`,
+                        right: `${proxEstado[0].right}px`
+                    });
                     capi.posicao = estadoAtual[0].norte;
-                    capiIMG.style.top = `${proxEstado[0].top}px`
-                    capiIMG.style.right = `${proxEstado[0].right}px`
                 }
 
                 break;
@@ -274,9 +44,12 @@ input.addEventListener('keypress', (event) => {
 
                 
                 if(estadoAtual[0].sul){
+                    caminho.push({
+                        posicao: estadoAtual[0].sul,
+                        top: `${proxEstado[0].top}px`,
+                        right: `${proxEstado[0].right}px`
+                    });
                     capi.posicao = estadoAtual[0].sul;
-                    capiIMG.style.top = `${proxEstado[0].top}px`
-                    capiIMG.style.right = `${proxEstado[0].right}px`
                 }
 
                 break;
@@ -287,9 +60,12 @@ input.addEventListener('keypress', (event) => {
 
                 
                 if(estadoAtual[0].leste){
-                    capi.posicao = estadoAtual[0].leste;
-                    capiIMG.style.top = `${proxEstado[0].top}px`
-                    capiIMG.style.right = `${proxEstado[0].right}px`
+                    caminho.push({
+                        posicao: estadoAtual[0].leste,
+                        top: `${proxEstado[0].top}px`,
+                        right: `${proxEstado[0].right}px`
+                    });
+                    capi.posicao = estadoAtual[0].lest;
                 }
 
                 break;
@@ -300,31 +76,32 @@ input.addEventListener('keypress', (event) => {
 
                 
                 if(estadoAtual[0].oeste){
+                    caminho.push({
+                        posicao: estadoAtual[0].oeste,
+                        top: `${proxEstado[0].top}px`,
+                        right: `${proxEstado[0].right}px`
+                    });
                     capi.posicao = estadoAtual[0].oeste;
-                    capiIMG.style.top = `${proxEstado[0].top}px`
-                    capiIMG.style.right = `${proxEstado[0].right}px`
                 }
 
                 break;
+            case "andar":
+                function andarComIntervalo(index) {
+                    if (index < caminho.length) {
+                        andar(caminho[index]); 
+                        setTimeout(() => andarComIntervalo(index + 1), 1000); // Chama a próxima iteração após 1s
+                    }
 
-            case "sudeste":
-                insereDirecao(valorDigitado);
-                break;
-            case "sudoeste":
-                insereDirecao(valorDigitado);
-                break;
-            case "nordeste":
-                insereDirecao(valorDigitado);
-                break;
-            case "noroeste":
-                insereDirecao(valorDigitado);
+                    if(capi.posicao == "GO")
+                        abrir()
+                }
+            
+                andarComIntervalo(0);
                 break;
             default:
                 alert("Direção invalida!")
 
         }
-        if(capi.posicao == "GO")
-            abrir()
     }
 });
 
@@ -333,4 +110,11 @@ function insereDirecao(direcao){
     input.value = '';
     let divDirecoes = document.getElementById('direcoes');
     divDirecoes.innerHTML = `<p class='roboto-regular'>${direcao.toUpperCase()}</p>` + divDirecoes.innerHTML;
+}
+
+function andar(e){
+    capi.posicao = e.posicao;
+    capiIMG.style.top = e.top
+    capiIMG.style.right = e.right;
+    input.value = ''
 }
